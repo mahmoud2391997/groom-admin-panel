d75b3372d29d373518cc2c4611de6d797fdc58cf
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2024-12-19 00:08:49 +0200

    solving deploying bug

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,16 +1,16 @@
-var admin = require("firebase-admin");
+// var admin = require("firebase-admin");
 
-var serviceAccount = require("../../serviceAccountKey.json");
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-}
-export default async function handler(req, res) {
-  const db = admin.firestore();
-  const snapshot = await db.collection("Parents").get();
-  const users = snapshot.docs.map((doc) => doc.data());
-  console.log(users);
+// var serviceAccount = require("../../serviceAccountKey.json");
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//   });
+// }
+// export default async function handler(req, res) {
+//   const db = admin.firestore();
+//   const snapshot = await db.collection("Parents").get();
+//   const users = snapshot.docs.map((doc) => doc.data());
+//   console.log(users);
 
-  res.status(200).json(users);
-}
+//   res.status(200).json(users);
+// }
